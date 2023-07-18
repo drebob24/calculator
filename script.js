3047c5aac9b772419d9e2a87ecbd4ad98ccf2959
drebob24 <abobbit@gmail.com>
2023-07-18 17:42:00 +0300

    Create functions to handle button inputs
    Calculator will perform basic math (plus, minus, multiply, divide) and
    performs consecutive operations (5 + 5 - 6 / 7 etc).
    
    Special functions need to be added as well as handling divide by 0.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let num1 = 0;
 let num2 = 0;
 let operator = '';
+let input1 = false;
+let input2 = false;
+let oper = false;
+
+const display = document.querySelector('.display');
+display.textContent = 0;
 
 function addition(x, y){
     return sum = x + y;
@@ -19,20 +25,81 @@ function division(x, y){
 }
 
 function operate(){
-    console.log("hi");
-
+    num1 = Number(num1);
+    num2 = Number(num2);
     switch(operator){
         case '+':
-            addition(num1, num2)
+            num1 = addition(num1, num2)
             break;
-        case '-':
-            subtraction(num1, num2);
+        case '−':
+            num1 = subtraction(num1, num2);
             break;
-        case '*': 
-            multiplication(num1, num2);
+        case '×': 
+            num1 = multiplication(num1, num2);
             break;
-        case '/': 
-            division(num1, num2);
+        case '÷': 
+            num1 = division(num1, num2);
             break;
+    }
+}
+
+const btns = document.querySelectorAll('button');
+btns.forEach(input => {
+    input.addEventListener('click', (e) => {
+        let inputClass = e.target.classList.value;
+        let inputText = e.target.textContent;
+        evaluateInput(inputClass, inputText);
+    })
+})
+
+function evaluateInput(type, text){
+    switch(type){
+        case 'num':
+            evaluateNumberInput(text);
+            break;
+        case 'oper':
+            operator = text;
+            oper = true;
+            input1 = true;
+            break;
+        case 'equal':
+            evaulateEqualsInput();
+            break;
+    }
+}
+
+function evaulateEqualsInput(){
+    if (input2){
+        operate();
+        display.textContent = num1;
+        input1 = false;
+        input2 = false;
+        oper = false;
+    }
+    else {
+        display.textContent = num1;
+        input1 = false;
+        oper = false;
+    }
+}
+
+function evaluateNumberInput(value){
+    if (!input1){
+        num1 = value;
+        display.textContent = num1;
+        input1 = true;
+    }
+    else if (!oper){
+        num1 += value;
+        display.textContent = num1;
+    }
+    else if (!input2){
+        num2 = value;
+        display.textContent = num2;
+        input2 = true;
+    }
+    else {
+        num2 += value;
+        display.textContent = num2;
     }
 }
