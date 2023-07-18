07a76613713b76bb1dfa42b1ea8cfeb6dc1fcfc6
drebob24 <abobbit@gmail.com>
2023-07-18 23:29:47 +0300

    Clean and Simplify code further
    evaluateEqualsInput function was redundant by cleaning up the
    operate function and realizing the if condition was not neeeded.
    
    Other simplifications of the code largely around replacing redundant
    logic in if/else statements that could be handled by 1 ternary
    operator at the end. Again helped identify these areas with AI and
    then verified the functionality.
    
    Used as a practice to get better at making efficient use of the ternary
    operator.

--- script.js
+++ script.js
@@ -3,55 +3,29 @@ let num1, num2, operator, input1, input2, oper, decimal;
 const display = document.querySelector('.display');
 display.textContent = 0;
 
-function addition(x, y){
-    return x + y;
-}
-
-function subtraction(x, y){
-    return x - y;
-}
-
-function multiplication(x, y){
-    return x * y;
-}
-
-function division(x, y){
-    if (y === 0){
-        resetCalc();
-        return "Invalid";
-    }
-    return x / y;
-}
+const addition = (x,y) => x + y;
+const subtraction = (x, y) => x - y;
+const multiplication = (x, y) => x * y;
+const division = (x, y) => y !== 0 ? x / y : "Invalid";
 
 function operate(){
     num1 = Number(num1);
     num2 = Number(num2);
-    switch(operator){
-        case '+':
-            num1 = addition(num1, num2)
-            break;
-        case '−':
-            num1 = subtraction(num1, num2);
-            break;
-        case '×': 
-            num1 = multiplication(num1, num2);
-            break;
-        case '÷': 
-            num1 = division(num1, num2);
-            break;
-    }
+    num1 =  operator === '+' ? addition(num1, num2) :
+            operator === '−' ? subtraction(num1, num2) :
+            operator === '×' ? multiplication(num1, num2) :
+            operator === '÷' ? division(num1, num2) :
+            num1;
     display.textContent = num1;
     decimal = false;
-    if (num1 === 'Invalid'){
-        num1 = num2 = 0;
-    }
+    num1 === 'Invalid' ? num1 = num2 = operator = 0 : num1;
 }
 
 const btns = document.querySelectorAll('button');
 btns.forEach(input => {
     input.addEventListener('click', (e) => {
-        let inputClass = e.target.classList.value;
-        let inputText = e.target.textContent;
+        const inputClass = e.target.classList.value;
+        const inputText = e.target.textContent;
         evaluateInput(inputClass, inputText);
     })
 })
@@ -65,7 +39,8 @@ function evaluateInput(type, text){
             handleOperaters(text);
             break;
         case 'equal':
-            evaulateEqualsInput();
+            operate();
+            input1 = input2 = oper = false;
             break;
         case 'special':
             handeSpecial(text);
@@ -73,45 +48,31 @@ function evaluateInput(type, text){
     }
 }
 
-function evaulateEqualsInput(){
-    if (input2){
-        operate();
-        input1 = input2 = oper = false;
-    }
-    else {
-        display.textContent = num1;
-        input1 = oper = false;
-    }
-}
-
 function evaluateNumberInput(value){
     if (!input1){
         num1 = value;
-        display.textContent = num1;
         input1 = true;
     }
     else if (!oper){
         num1 += value;
-        display.textContent = num1;
     }
     else if (!input2){
         num2 = value;
-        display.textContent = num2;
         input2 = true;
     }
     else {
         num2 += value;
-        display.textContent = num2;
     }
+    display.textContent = input2 ? num2 : num1;
 }
 
 function handleOperaters(sign){
-    if(!oper){
+    if (!oper) {
         operator = sign;
         oper = input1 = true;
         decimal = false;
     }
-    else{
+    else {
         operate();
         operator = sign;
         input2 = false;
@@ -148,21 +109,19 @@ function resetCalc(){
 function handleBackspace(){
     if (input2){
         num2 = num2.slice(0, -1);
-        display.textContent = num2;
     }
     else {
         num1 = num1.slice(0, -1);
-        display.textContent = num1;
     }
+    display.textContent = input2 ? num2 : num1;
 }
 
 function handlePercentage(){
     if (input2){
         num2 = num2/100;
-        display.textContent = num2;
     }
     else {
         num1 = num1/100;
-        display.textContent = num1;
     }
+    display.textContent = input2 ? num2 : num1;
 }
